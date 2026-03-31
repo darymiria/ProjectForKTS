ed03b5078f55f63ebdf8dd7a67e4d0d711eac000
Daria <dashulya0445@gmail.com>
2026-03-31 18:23:10 +0300

    Выполнение ДЗ7

---
+++ composeApp/src/androidMain/kotlin/com/example/projectforkts/CrashlyticsAntilog.kt
@@ -0,0 +1,18 @@
+package com.example.projectforkts
+
+//
+//import com.google.firebase.crashlytics.FirebaseCrashlytics
+//import io.github.aakira.napier.Antilog
+//import io.github.aakira.napier.LogLevel
+//
+//class CrashlyticsAntilog : Antilog() {
+//    private val crashlytics = FirebaseCrashlytics.getInstance()
+//    override fun performLog(
+//        priority: LogLevel,
+//        tag: String?,
+//        throwable: Throwable?,
+//        message: String?
+//    ) {
+//        TODO("Not yet implemented")
+//    }
+//}
